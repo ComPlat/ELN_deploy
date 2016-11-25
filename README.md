dceeab4b581080cb4a1e448468d128659bfd4a9b
Serhii Kotov <s.kotovskij@gmail.com>
2016-11-25 17:48:50 +0100

    Initial recipes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ELN_deploy
+
+This is a chef recipe to install Chemotion ELN to server
+Rough list of commands to use
+* apt-get install ruby
+* gem install bundler
+* bundle
+* Important: edit user and passwords first!
+* knife solo prepare <user>@<IP or domain>
+* knife solo cook <user>@<IP or domain>
---
+++ site-cookbooks/chemotion/metadata.rb
@@ -0,0 +1,14 @@
+name             'chemotion'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chemotion'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'rvm'
+depends 'phusionpassenger'
+depends 'users'
+depends 'postgresql'
+depends 'nvm'
